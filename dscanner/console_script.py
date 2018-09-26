690d8488170fb96d51a7292ee4ca714ce62d81c5
toshs <33202669+toshs@users.noreply.github.com>
2018-09-27 05:35:07 +0900

    [update] use qr module

--- dscanner/console_script.py
+++ dscanner/console_script.py
@@ -1,8 +1,8 @@
 import sys
+from . import qr
 
 def main():
     domain = sys.argv[1]
-    domain = domain.split('.')
-    
-    for i, d in enumerate(domain[::-1]):
-        print(i+1, "th domain is", d)
+
+    print("searching urls have near codeword of QR...")
+    print(qr.near_urls(domain))
