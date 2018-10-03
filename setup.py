0285829e4a8506254eabe622d3a5bf2bd24e974b
TakayukiTomatsuri <TakayukiTomatsuri@users.noreply.github.com>
2018-10-03 17:19:48 +0900

    [update] add information about showing progress to setup.py, README.md #20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="dscanner",
     version="1.0.0",
-    install_requires=["requests", "qrcode", "tldextract", "beautifulsoup4", "gglsbl"],
+    install_requires=["requests", "qrcode", "tldextract", "beautifulsoup4", "gglsbl", "tqdm"],
     entry_points={
         "console_scripts": [
             "dscan=dscanner.console_script:main",
